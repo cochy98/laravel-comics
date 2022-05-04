57a7cca12e47a4865092e72689cda1910a3b78c1
cochy98 <cosmoferrigno@gmail.com>
2022-05-04 17:59:12 +0200

    Add header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,43 @@
+<header>
+  <div class="container">
+    <nav>
+      <img
+        src="images/dc-logo.png"
+        class="header-logo"
+        alt="DC Logo"
+      />
+      <ul>
+        <li>
+          <a href="/">Characters</a>
+        </li>
+        <li class="active">
+          <a href="/">Comics</a>
+        </li>
+        <li>
+          <a href="/">Movies</a>
+        </li>
+        <li>
+          <a href="/">Tv</a>
+        </li>
+        <li>
+          <a href="/">Games</a>
+        </li>
+        <li>
+          <a href="/">Collectibles</a>
+        </li>
+        <li>
+          <a href="/">Videos</a>
+        </li>
+        <li>
+          <a href="/">Fans</a>
+        </li>
+        <li>
+          <a href="/">News</a>
+        </li>
+        <li>
+          <a href="/">Shop</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
