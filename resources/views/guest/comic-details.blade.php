06a3fe46c5e5d64c947713d245c55d190d4fdc78
cochy98 <cosmoferrigno@gmail.com>
2022-05-06 16:11:57 +0200

    Modify 'comic-details'

--- resources/views/guest/comic-details.blade.php
+++ resources/views/guest/comic-details.blade.php
@@ -6,21 +6,41 @@
 <section id="main-content">
   <div class="jumbotron">
     <img src="{{ asset('images/jumbotron.jpg') }}" alt="jumbotron" />
-    <div class="end-section">
-      <div class="container">
-        <div class="item-cover">
-          <span>{{ $comic['type'] }}</span>
-          <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-          <span>View gallery</span>
-        </div>
+  </div>
+
+  <div class="start-details">
+    <div class="container">
+      <div class="item-cover">
+        <span>{{ $comic['type'] }}</span>
+        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+        <span>View gallery</span>
       </div>
     </div>
   </div>
-  <div class="container-details">
 
-    @dump($comic)
+  <div class="comic-details">
+    <div class="container">
+      <div class="col-left">
+        <h1 class="comic-title">{{ $comic['title'] }}</h1>
+        <p class="comic-description">{{ $comic['description'] }}</p>
+        @dump($comic)
+      </div>
 
+      <div class="col-right">
+        <img src="{{ asset('images/pubb.jpg') }}" alt="ADV photo">
+      </div>
+    </div>
+  </div>
 
+  <div class="comic-specs">
+    <div class="container">
+      <div class="col-left">
+        <h1>Talent</h1>
+      </div>
+      <div class="col-right">
+        <h1>Specs</h1>
+      </div>
+    </div>
 
   </div>
 </section>
